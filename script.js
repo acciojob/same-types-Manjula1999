4f9f6c54830484f6d1c71271fe5fca14de393a79
Manjula1999 <144819647+Manjula1999@users.noreply.github.com>
2024-09-03 00:14:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function isSameType(value1, value2) {
 	if( isNaN(value1) &&  isNaN(value2)){
 		return true;
 	}
-if else(isNaN(value1) || isNaN(value2)){
+ else if(isNaN(value1) || isNaN(value2)){
 	return false;
 }
 		return typeof value1 === typeof value2;
